612ceacf02968b0bc916b03d2a4b0a608bf815f8
BhrAri <128198729+BhrAri@users.noreply.github.com>
2025-07-07 16:33:29 -0600

    finished

--- average.py
+++ average.py
@@ -3,9 +3,9 @@
 #Date: 6/30/2025
 #Description: Takes 5 user number inputs and averages them
 print ("Please enter 5 numbers for me to average")
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-e = int(input())
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
 print((a + b + c + d + e)/5)
--- temp_convert.py
+++ temp_convert.py
@@ -2,6 +2,7 @@
 #GitHub Username: Student40835
 #Date: 6/30/2025
 #Description: Converts Celsius to Fahrenheit
-c_temp = float(input("Please enter a Celsius temperature"))
+c_temp = float(input("Please enter a Celsius temperature."))
 f_temp = (((9/5) * c_temp) + 32)
-print ("The temperature in Fahrenheit is", f_temp)
+print("The equivalent Fahrenheit temperature is:")
+print (f_temp)
