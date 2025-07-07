0e72b8956856996e33afbbd9fa2d2860b3634a6e
BhrAri <128198729+BhrAri@users.noreply.github.com>
2025-07-07 16:44:05 -0600

    finished

--- min_max.py
+++ min_max.py
@@ -3,10 +3,13 @@
 #Date: 7/7/2025
 #Description: Allows entry of a specified number of integers. Prints the smallest and largest entered values.
 print("How many integers would you like to enter?")
-minInteger = 0
-maxInteger = 0
 numberOfIntegers = int(input())
 print("Please enter " + str(numberOfIntegers) + " integers.")
+if numberOfIntegers != 0:
+    temporaryInteger = int(input())
+    minInteger = temporaryInteger
+    maxInteger = temporaryInteger
+    numberOfIntegers -= 1
 while numberOfIntegers > 0:
     temporaryInteger = int(input())
     if temporaryInteger > maxInteger : maxInteger = temporaryInteger
