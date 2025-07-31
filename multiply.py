b98c73a324b1aacecf92894e1bb2a0b17c2cc8c4
BhrAri <128198729+BhrAri@users.noreply.github.com>
2025-07-30 20:09:49 -0600

    finished

---
+++ multiply.py
@@ -0,0 +1,12 @@
+#Author: Maxwell Benjamin
+#GitHub Username: Student40835
+#Date: 7/30/2025
+#Description: a recursive function named multiply that takes two positive integers as parameters and returns the product of those two numbers
+number_3=0
+def multiply(number_one,number_2):
+    if number_2 == 0:
+        return 0
+    if number_2 == 1:
+        return number_one
+    if number_2 >= 2:
+        return number_one + multiply(number_one,number_2-1)
